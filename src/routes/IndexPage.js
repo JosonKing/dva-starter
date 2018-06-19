5b92789303267a06842f201705abd8413430e8a1
josonking <wangsanmu5@163.com>
2018-06-20 00:13:14 +0800

    add user.jsx table data

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'dva';
 import styles from './IndexPage.css';
 import User from '../components/user';
 
-function IndexPage() {
-  return (
-    <div className={styles.container}>
-      hello
-      <User />
-    </div>
-  );
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  // componentDidMount() {
+  //   const { dispatch } = this.props;
+  //   dispatch({ type: 'init', payload : null });
+  // }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <User />
+      </div>
+    );
+  }
 }
 
-IndexPage.propTypes = {
-};
-
-export default connect()(IndexPage);
+export default IndexPage;
