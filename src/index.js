a51b12e25c998cc1d81f7711b4058f607b82ac19
josonking <wangsanmu5@163.com>
2018-06-22 17:06:44 +0800

    edit index ruter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+import 'babel-polyfill';
 import dva from 'dva';
+import { message } from 'antd';
 import './index.css';
+import createLoading from 'dva-loading';
 
 // 1. Initialize
-const app = dva();
+const app = dva({
+  onError(e, dispatch) {
+    console.log("error!", e.message);
+    message.error(e.message);
+  },
+});
 
 // 2. Plugins
 // app.use({});
+app.use(createLoading());
 
 // 3. Model
 app.model(require('./models/user').default);
--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
+import { Router, Route, Switch, IndexRoute } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
+        {/* <IndexRoute component={IndexPage} /> */}
         <Route path="/" exact component={IndexPage} />
       </Switch>
     </Router>
